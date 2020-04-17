32659ee2fdd1e0cda2163bc23928a6dc3eac6f57
Ananias Lima <ananias.lima@hotmail.com>
2020-04-16 23:50:07 -0300

    Coloquei um vetor de imagens

--- app/src/main/java/com/example/applayout/MainActivity.kt
+++ app/src/main/java/com/example/applayout/MainActivity.kt
@@ -5,8 +5,10 @@ import android.app.ActivityManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.URLUtil
 import android.widget.MediaController
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
@@ -17,40 +19,79 @@ class MainActivity : AppCompatActivity() {
     var position = 0
     var mcontroller : MediaController?=null
     var ultimaMemoria: Long = 0
+    lateinit var videoArray: ArrayList<String>
+    var lastPlayedVideo: Int = -1
 
-    private val VIDEO_NAME = "v100"
 
-
-    private fun getURI(videoname:String): Uri{
+    private fun getURI(videoname:String): Uri {
         if (URLUtil.isValidUrl(videoname)) {
             //  an external URL
-            return Uri.parse(videoname);
+            return Uri.parse(videoname)
         } else { //  a raw resource
             return Uri.parse("android.resource://" + getPackageName() +
                     "/raw/" + videoname);
         }
     }
 
+
+    private fun prepareVideoArray() {
+        videoArray = arrayListOf()
+        videoArray.add("v100")
+        videoArray.add("v15")
+        videoArray.add("v01")
+        videoArray.add("v201")
+        videoArray.add("v202")
+    }
+
+    private fun playNextVideo() {
+        if (videoArray.isEmpty()) {
+            return
+        }
+
+        lastPlayedVideo++
+
+        if (lastPlayedVideo == videoArray.size ) {
+            lastPlayedVideo = 0
+        } else {
+            if ( videoArray[lastPlayedVideo].isEmpty() ) {
+                lastPlayedVideo = 0
+            }
+        }
+
+        println("WWW PLAYING NEXT VIDEO $lastPlayedVideo  Video:${videoArray[lastPlayedVideo]} Max:${videoArray.size}")
+
+//        videoView.setVideoPath(getURI(videoArray[lastPlayedVideo]))
+        videoView.setVideoURI(getURI(videoArray[lastPlayedVideo]))
+
+        videoView.start()
+    }
+
+
     private fun initPlayer() {
-        var videoUri:Uri = getURI(VIDEO_NAME)
         var poucaMemoria = ActivityManager.MemoryInfo().lowMemory
 
         if (poucaMemoria ) {
             Timber.w("== == == == == == == == POUCA MEMORIA == == == == == == == ==")
         }
 
-        videoView.setVideoURI(videoUri)
+        videoView.setVisibility(View.VISIBLE)
+
+        lastPlayedVideo = 0
+        videoView.setVideoURI(getURI(videoArray[lastPlayedVideo]))
+
+//        videoView.setVideoPath(getURI(videoArray[lastPlayedVideo]))
 
         videoView.setOnCompletionListener {
-            videoView.seekTo(1);
-            videoView.start()
-            var memoria = Runtime.getRuntime().freeMemory()
+            try {
+                playNextVideo()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
 
+            var memoria = Runtime.getRuntime().freeMemory()
             Timber.i("---Antes: %d   Depois de liberar: =%d  diferença: %d", ultimaMemoria, memoria, (memoria - ultimaMemoria))
             ultimaMemoria = memoria
-
         }
-
 
         videoView.start()
     }
@@ -88,6 +129,8 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        prepareVideoArray()
+
         btnStart.setOnClickListener  {
             Timber.i("WWW Vai chamar initPlayer...")
             initPlayer()
@@ -101,9 +144,11 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        videoView.setOnClickListener {
+        btnInvisivel.setOnClickListener {
             Timber.i("WWW Vai chamar releasePlayer...")
             releasePlayer()
+            videoView.setVisibility(View.INVISIBLE)
+
             Timber.i("WWW chamou releasePlayer")
         }
     }
